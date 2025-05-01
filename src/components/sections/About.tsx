9f6bf04a6b9188403fd1586e6248a153334a6d82
Parthp55 <vrhorizon7@gmail.com>
2025-05-01 23:20:27 +0530

    sdgdhyfj

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -16,7 +16,7 @@ const About: React.FC = () => {
       {/* Background Pattern */}
       <div className="absolute inset-0 opacity-5 pointer-events-none">
         <div className="absolute inset-0" style={{
-          backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+          backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns=''%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
           backgroundSize: '60px 60px'
         }}/>
       </div>
@@ -107,7 +107,7 @@ const About: React.FC = () => {
               {/* Decorative Element */}
               <div className="absolute -bottom-6 -left-6 h-24 w-24 rounded-lg overflow-hidden shadow-lg z-20">
                 <img 
-                  src="https://images.pexels.com/photos/5858235/pexels-photo-5858235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                  src="https://www.creativehatti.com/wp-content/uploads/edd/2021/05/Banner-design-top-in" 
                   alt="Astrology Tools" 
                   className="w-full h-full object-cover"
                 />
